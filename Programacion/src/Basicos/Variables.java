c046bd0b3ead4bc96d3ce70f696a08735df14c5f
CMaster <CMaster@DESKTOP-K2CQ5G0>
2020-10-28 16:15:56 +0100

    Parimpar y las multis y variables

---
+++ Programacion/src/Basicos/Variables.java
@@ -0,0 +1,38 @@
+package Basicos;
+
+public class Variables {
+		public static void main(String[] args) {
+			
+			//enteros
+			int entero;
+			//deciamles
+			float decimalcorto;
+			double decimalgrande;
+			//caracteres,ocupan 2 bytes
+			char letra = "e";
+			//bytes
+			byte b;
+			
+			//bit 0 y 1
+			//Byte = 8 bits, ej 000100010
+			
+			//cadenas de caracteres
+			String cadena = "Hola clase";
+			//Decalración
+			
+			//Inicaialización
+			//Asignación del valor
+			//Casting
+			b = (byte) entero;
+			System.out.print(b);
+			System.out.println(decimalcorto);
+			System.out.print((int)decimalcorto);
+			String numero ="100";
+			System.out.println(Integer.parseInt(numero));
+			//Operaciones con variables +,-,*,/,%
+			//
+			
+		}
+
+	}
+
---
+++ Programacion/src/UT3/Parimpar.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package UT3;
+
+/**
+ * @author CMaster
+ *
+ */
+public class Parimpar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	    int numero=9; 
+
+	    if (numero%2==0) 
+
+	    { 
+
+	    	System.out.println("es par"); 
+
+	    } else { 
+
+	    	System.out.println("es impar"); 
+
+   }
+
+	}
+
+}
